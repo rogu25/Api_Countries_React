6755fae4d9fce9e2da7d7fa7bad862387bbd8637
rogu25 <ryavar.montesinos@gmail.com>
2023-12-07 10:13:44 -0500

    CLIENT: paginado countries, terminado

--- client/src/components/Countries.js
+++ client/src/components/Countries.js
@@ -1,23 +1,162 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+import { get_all_countries } from "../redux/action";
 
 import CardCountry from './CardCountry';
 
 import "../css/Countries.css";
+import Pagination from './Pagination';
 
 function Countries() {
 
-    const countries = useSelector((state) => state.countries)
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries);
+
+  const [backcountries, setBackCountries] = useState(0);
+  const [nextcountries, setNextCountries] = useState(9);
+  const [backpage, setBackPage] = useState(0);
+  const [nextpage, setNextPage] = useState(5);
+
+  const [id, setId] = useState(1);
+
+  // const [activo, setActivo] = useState(false);
+
+  let contadorCountries = countries.length;
+  let contadorPage = Math.ceil(countries.length / 9.9);
+  const items = 10;
+  const page = 5;
+
+  const elementos = Array.from({ length: contadorPage }, (_, index) => index + 1);
+
+  const onclickSelectPage = (id) => {
+
+    if (id === 1) {
+      setBackCountries((items * id) - items);
+      setNextCountries((items * id) - 1);
+      setId(id);
+      return
+    }
+
+    setBackCountries(((items * id) - items) - 1);
+    setNextCountries((items * id) - 1);
+    setId(id);
+
+  }
+
+  const onclickBackPage = () => {
+
+    if (id <= (backpage + 1)) return
+
+    if (id <= 1) return
+
+    if (backcountries <= 9) {
+      setBackCountries((backcountries - items) + 1);
+      setNextCountries(nextcountries - items);
+      setId(id - 1)
+      return
+    }
+
+    setId(id - 1);
+    setBackCountries(backcountries - items);
+    setNextCountries(nextcountries - items);
+
+  }
+
+  const onclickNextPage = () => {
+
+    if (id >= nextpage) return
+
+    if (backcountries < 9) {
+      setBackCountries((backcountries + items) - 1);
+      setNextCountries(nextcountries + items);
+      setId(id + 1)
+      return
+    }
+
+    if (nextcountries >= contadorCountries) return
+
+    setBackCountries(backcountries + items);
+    setNextCountries(nextcountries + items);
+    setId(id + 1)
+
+  }
+
+  const onclickChangeBackPage = () => {
+    
+    if (backpage < 1) return
+    
+    if ((id % page) === 1) {
+      setId(id - page);
+      
+    }else if((id % page) === 0){
+      setId(((id - page) - page) + 1);
+    }else{
+      setId(((id - page) - (id % page)) + 1);
+    }
+    setBackPage(backpage - page);
+    setNextPage(nextpage - page);
+  };
+
+  const onclickChangeNextPage = () => {
+    if (nextpage >= contadorPage) return
+
+    if (id > contadorPage) return
+
+    if (id === (contadorPage - 1)) {
+      setBackPage(backpage + page);
+      setNextPage(nextpage + 1);
+      setId(id + 1);
+      return
+    }
+
+    if ((id % page) === 1) {
+      setId(id + page);
+      
+    }else if((id % page) === 0) {
+      setId(id + 1);
+    }else{
+      setId(((id + page) - (id % page)) + 1);
+    }
+    setBackPage(backpage + page);
+    setNextPage(nextpage + page);
+    setBackCountries(((items * id) - items) - 1);
+    setNextCountries((items * id) - 1);
+  };
+
+  const onclickCallBack = useCallback(() => {
+    if (id === 1) {
+      setBackCountries((items * id) - items);
+      setNextCountries((items * id) - 1);
+      setId(id);
+      return
+    }
+    setBackCountries(((items * id) - items) - 1);
+    setNextCountries((items * id) - 1);
+    setId(id);
+  }, [id]);
+
+  useEffect(() => {
+    !countries.length && dispatch(get_all_countries());
+  }, [countries, dispatch]);
 
 
   return (
-    <div  className='content_countries'>
+    <React.Fragment>
+
+      <Pagination elementos={elementos} inicio={backpage} final={nextpage} id={id} onclickSelectPage={onclickSelectPage} onclickNextPage={onclickNextPage} onclickBackPage={onclickBackPage} onclickChangeNextPage={onclickChangeNextPage} onclickChangeBackPage={onclickChangeBackPage} onclickCallBack={onclickCallBack} />
+
+      <div className='content_countries'>
         {
           countries.length && countries.map((c) => {
-            return <CardCountry key={c.id} id={c.id} imagen={c.imagen} nombre={c.name} continente={c.continente} poblacion={c.poblacion}/>
-          })
+            return (
+              <CardCountry key={c.id} id={c.id} imagen={c.imagen} nombre={c.name} continente={c.continente} poblacion={c.poblacion} />
+            )
+          }).slice(backcountries, nextcountries)
         }
-    </div>
+      </div>
+
+    </React.Fragment>
   )
 }
 
